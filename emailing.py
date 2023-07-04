0220ab0c5ffac118d34d3106ce687c5b1b520f4c
doctorwho1110 <yeldanugur99@gmail.com>
2023-07-04 10:36:12 +0300

    last commit

--- emailing.py
+++ emailing.py
@@ -1,2 +1,24 @@
-def send_email():
-    print("Email was sent!")
+import imghdr
+import smtplib
+from email.message import EmailMessage
+
+SENDER = "your_email_adress"
+PASSWORD = "your_gmail_app_password"
+RECEIVER = "your_email_adress"
+
+
+def send_email(image_path, ):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
